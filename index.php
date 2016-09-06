7cde23815436a6b2946b76f3bb63cdcfff0935d9
gabriel <gabriel@gabriel-PC>
2016-09-06 01:30:24 -0300

    Se puso la tabla default y corrigio rutas de Scritp

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+<!-- Estilos CSS vinculados -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	    <link href="css/estilos.css" rel="stylesheet">
+
+	<title>Panel Pakua</title>
+</head>
+<body class="">
+<header class="">
+	 <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Lista de Navegacion</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Panel Pakua</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Alumnos</a>
+                    </li>
+                    <li>
+                        <a href="#">Maestros</a>
+                    </li>
+                    <li>
+                        <a href="#">Eventos</a>
+                    </li>
+                </ul>
+
+                 <button type="button" class="btn btn-info navbar-btn navbar-right"> <span class="glyphicon glyphicon-user"> </span> Login</button> <!-- Boton LOGIN -->
+            </div>
+            <!-- /.navbar-collapse -->
+
+
+
+        </div>
+        <!-- /.container -->
+    </nav>
+
+   
+	
+
+	</header>
+
+<div class="container"> <!-- div container inicial -->
+<div class="table-responsive">
+  
+    <table class="table table-hover">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+  
+</div>
+ </div> <!-- Fin del container principal-->
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-dropdown.js"></script>
+<script type="text/javascript" src="js/scrollspy.js"></script>
+<script type="text/javascript" src="js/modal.js"></script>
+<script type="text/javascript" src="js/classie.js"></script>
+<script type="text/javascript" src="js/cbpAnimatedHeader.js"></script>
+<script type="text/javascript" src="js/transition.js"></script>
+<script type="text/javascript" src="js/collapse.js"></script>
+<script type="text/javascript" src="js/npm.js"></script>
+	
+
+	
+<footer class="panel-footer">
+
+<div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="">Copyright &copy; tu sitio web 2016</span>
+                </div>
+                 <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li><a href="https://www.facebook.com/gabrieleduardo.cano.5" target="_blank"><img alt="Siguenos en Facebook" src="https://lh6.googleusercontent.com/-CYt37hfDnQ8/T3nNydojf_I/AAAAAAAAAr0/P5OtlZxV4rk/s32/facebook32.png" width=32 height=32 >
+						</li></a>
+
+						    <li><a rel="author" href="https://plus.google.com/TU_ID_GOOGLE?rel=author" target="_blank"><img alt="Siguenos en Google+" src="https://lh5.googleusercontent.com/-F_FBKktZWZk/T3nN0v4FeAI/AAAAAAAAAsc/7dHTxkxn4cI/s32/google32.png" width=32 height=32 ></a>
+
+						</li></a>
+
+						    <li><a href="http://twitter.com/AQUI_TU_USUARIO" target="_blank"><img src="https://lh6.googleusercontent.com/--aIk2uBwEKM/T3nN1x09jBI/AAAAAAAAAs8/qzDsbw3kEm8/s32/twitter32.png" width=32 height=32 alt="Síguenos en Twitter">
+						</li></a>
+                       
+                        
+                      
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul class="">
+                        <li class="	"><a href="#">Politicas de privacidad</a>
+                        </li>
+                        <li class=""><a href="#">Terminos de uso</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+ 
+</footer>
+
+</body>
+
+
+</html>
